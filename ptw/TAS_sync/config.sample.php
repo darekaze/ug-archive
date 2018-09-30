00c0a799384c90b293a42527a74e0d41a2fac1f9
DaRekaze <darekaze@gmail.com>
2018-09-30 23:31:03 +0800

    Hide config

--- ptw/TAS_sync/config.sample.php
+++ ptw/TAS_sync/config.sample.php
@@ -1,23 +1,19 @@
 <?php
 // To include, use: $configs = include('config.php');
-
 return array(
-    'TASusername' => xxx,
-    'TASpassword' => xxx,
+    'TASusername' => '',
+    'TASpassword' => '',
 
     'TASdb_driver' => 'oracle.jdbc.driver.OracleDriver',
-    'TASdb_server' => 'xxx.comp.polyu.edu.hk',
+    'TASdb_server' => '',
     'TASdb_port' => '1521',
     'TASdb_sid' => 'xxx',
     
-    // RBS Synchronizer
+    // RBS Stuff
     'rbsDriver' => 'com.mysql.jdbc.Driver',
-
-    // Production
     'rbsConnectionString' => 'jdbc:mysql://myxxx.comp.polyu.edu.hk/rbs?user=><username>&password=><password>',
     'rbsURL' => 'https://xxx.comp.polyu.edu.hk/edit_entry_handler.php',
     'rbsLoginURL' => 'https://xxx.comp.polyu.edu.hk/admin.php',
-    'GIBURL' => 'https://xxx.comp.polyu.edu.hk/getindbooking.php',
 
     // TAS Syn Configuration
     'period' => '2018-2019',
@@ -27,17 +23,6 @@ return array(
     'start_year'=>'2016',
     'end_day'=>'16',
     'end_month'=>'4',
-    'end_year'=>'2016',
-
-    'emailFrom' => 'labbook@comp.polyu.edu.hk',
-    'emailTo' => 'csterence@comp.polyu.edu.hk,csevan@comp.polyu.edu.hk,helpdesk@comp.polyu.edu.hk',
-    'smtphost' => 'mailhost.comp.polyu.edu.hk',
-
-    // CLS DB Parameter
-    'cls.driver'   => 'oracle.jdbc.driver.OracleDriver',
-    'cls.url'      => 'jdbc:oracle:thin:@xxx.comp.polyu.edu.hk:1521:xxx',
-    'cls.account'  => 'compxxxx',
-    'cls.password' => 'xxxxxxxx'
-)
-
+    'end_year'=>'2016'
+);
 ?>
