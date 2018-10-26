c9b3d420782dda20d177432e27d187d4624f52ea
DaRwin <32747549+darekaze@users.noreply.github.com>
2018-10-26 17:42:15 +0800

    Update dict_att.py

--- courses/c3334-security/lab2/dict_att.py
+++ courses/c3334-security/lab2/dict_att.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, unicode_literals
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA
 
-_IV = 16 * '\x00'
+_IV = bytes([0] * 16)
 
 
 def genSHA(text):
@@ -10,25 +10,25 @@ def genSHA(text):
     return h.hexdigest()
 
 
-def aesEncrypt(key, text):
-    encryption = AES.new(key, AES.MODE_CBC, _IV)
-    byteCipher = encryption.encrypt(text)
-    return byteCipher.hex()
-
-
-def genKey(str):
+def genKey(text):
     tempKey = []
-    for i in range(0, len(str)//2-4):
-        hexTeam = str[i*2] + str[i*2+1]
+    for i in range(0, len(text)//2-4):
+        hexTeam = text[i*2] + text[i*2+1]
         tempKey.append(int(hexTeam, 16))
     return tempKey
 
 
-def cracker(str, r):
-    r = bytes(r, 'utf-8')
-    key = bytes(genKey(genSHA(str)))
-    encode = aesEncrypt(r, key)
-    print(str(encode))
+def aesEncrypt(key, text):
+    encryption = AES.new(key, AES.MODE_CBC, _IV)
+    textBytes = encryption.encrypt(text)
+    return textBytes.hex()
+
+
+def cracker(guess, cr):
+    r = bytes(cr, 'utf-8')
+    key = bytes(genKey(genSHA(guess)))
+    res = aesEncrypt(key, r)
+    print(res)
 
 
 def main():
